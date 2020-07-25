8e4b1dd3562d75b2d6e431f649d60e0049b7bafc
ryo <ryoutakaya3623@Yahoo.co.jp>
2020-07-25 22:27:58 +0900

    fixed production

--- knexfile.js
+++ knexfile.js
@@ -32,17 +32,13 @@ module.exports = {
 
   production: {
     client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
+    connection: process.env.DATABASE_URL,
     migrations: {
+      directory: "./db/migrations",
       tableName: "knex_migrations",
+    },
+    seeds: {
+      directory: "./db/seeds",
     },
   },
 };
