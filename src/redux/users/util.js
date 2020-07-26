89c956c1e63bcb812817b40dfedbf9167ee28829
ryo <ryoutakaya3623@Yahoo.co.jp>
2020-07-26 11:19:39 +0900

    add proxy and change hashRouter

--- src/redux/users/util.js
+++ src/redux/users/util.js
@@ -3,7 +3,7 @@ import * as actions from "./actions";
 export const sendLoginInfomation = (nickname, password) => {
   return (dispatch) => {
     axios
-      .post("http://localhost:3002/user/login", {
+      .post("/user/login", {
         data: { nickname, password },
       })
       .then((res) => {
@@ -15,22 +15,20 @@ export const sendLoginInfomation = (nickname, password) => {
   };
 };
 export const post_favorite_artist = (user_id, artist_name) => {
-  axios.post("http://localhost:3002/user/post/artist", {
+  axios.post("/user/post/artist", {
     data: { user_id, artist_name },
   });
 };
 export const delete_favorite_artist = (user_id, artist_name) => {
-  axios.delete("http://localhost:3002/user/delete/artist", {
+  axios.delete("/user/delete/artist", {
     data: { user_id, artist_name },
   });
 };
 export const get_user_favorite = (user_id) => {
   return (dispatch) => {
-    axios
-      .post("http://localhost:3002/user/favorite/artist", { data: { user_id } })
-      .then((res) => {
-        console.log(res);
-        dispatch(actions.get_favorite_artist(res.data));
-      });
+    axios.post("/user/favorite/artist", { data: { user_id } }).then((res) => {
+      console.log(res);
+      dispatch(actions.get_favorite_artist(res.data));
+    });
   };
 };
