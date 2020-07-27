c14258750ebde4abb34cf8513993c9dbb1a8257e
ryo <ryoutakaya3623@Yahoo.co.jp>
2020-07-27 13:05:25 +0900

    fixed login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,18 +1,39 @@
 import React, { useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import axios from "axios";
+import styled from "styled-components";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import { sendLoginInfomation } from "../redux/users/util";
-import { Redirect, Switch, Route } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStyle = makeStyles({
+  textInput: {
+    display: "block",
+    marginBottom: "40px",
+  },
+  button: {
+    display: "block",
+    margin: "0 auto",
+    // marginLeft: "100%",
+  },
+});
+
+const Title = styled.h2`
+  font-size: 1.5em;
+`;
+
+const Container = styled.div`
+  height: 50%;
+  width: 50%;
+  text-align: center;
+`;
 const Index = () => {
+  const styles = useStyle();
   const nickNameFeeld = useRef(null);
   const passWordFeeld = useRef(null);
   const dispatch = useDispatch();
 
   const loginFlag = useSelector((store) => store.users.loginFlag);
-  const checkInput = (nickName, password) => {};
 
   const loginFunc = () => {
     dispatch(
@@ -25,25 +46,34 @@ const Index = () => {
 
   return (
     <>
-      <div>
-        <h2>Login してください</h2>
+      <Container>
+        <Title>Login してください</Title>
         <TextField
+          fullWidth={true}
+          className={styles.textInput}
           inputRef={nickNameFeeld}
           id="standard-required"
           label="nickname"
         />
 
         <TextField
+          fullWidth={true}
+          className={styles.textInput}
           inputRef={passWordFeeld}
           id="standard-password-input"
           label="Password"
           type="password"
           autoComplete="current-password"
         />
-        <Button onClick={() => loginFunc()} variant="contained" color="primary">
+        <Button
+          className={styles.button}
+          onClick={() => loginFunc()}
+          variant="contained"
+          color="primary"
+        >
           Login
         </Button>
-      </div>
+      </Container>
     </>
   );
 };
